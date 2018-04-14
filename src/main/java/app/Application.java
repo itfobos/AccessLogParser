e116ef0716fc6822b0bceba53650d96cec7e20ba
ILya Turukin <tr.wolfer@gmail.com>
2018-04-14 19:34:11 +0700

    Added app proto. Spring Boot is used.

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,24 @@
+package app;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			//TODO:
+		};
+	}
+}
